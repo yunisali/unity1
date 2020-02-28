27bf284df8f4b1a8480499cc647e09bc00fd3a57
Cody Smith <c0dysmyth@gmail.com>
2020-02-27 21:12:34 -0500

    SCRIPTS
    SCRIPTS

---
+++ Assets/fly.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fly : MonoBehaviour
+{
+    public GameObject plane;
+    public float speed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+    }
+}
+
