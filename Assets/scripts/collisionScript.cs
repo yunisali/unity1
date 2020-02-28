fa9d381f3d0e90ae753dbde63642d75cda146aaa
Cody Smith <c0dysmyth@gmail.com>
2020-02-28 09:37:38 -0500

    particles
    particles

---
+++ Assets/scripts/collisionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour
+{
+    public Transform explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    public void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.tag.Equals("enemy"))
+        {
+            Instantiate(explosion, transform.position, explosion.rotation);
+            Destroy(col.gameObject);
+
+        }
+    }
+    }
